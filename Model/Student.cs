da55110a3638bb4f62de16f6e3ce62871ad8acb1
benxiong.hu <1301536601@qq.com>
2022-03-08 16:33:23 +0800

    New analog data generation

---
+++ Model/Student.cs
@@ -0,0 +1,53 @@
+﻿
+// ReSharper disable All
+/// <summary>
+/// Student Modle
+/// </summary>
+public class Student
+{
+    /// <summary>
+    /// Id
+    /// </summary>
+    public Guid Id { get; set; }
+    
+    /// <summary>
+    /// Name
+    /// </summary>
+    public string Name { get; set; }
+    
+    /// <summary>
+    /// Address
+    /// </summary>
+    public string  Address { get; set; }
+    
+    /// <summary>
+    /// Age
+    /// </summary>
+    public int Age { get; set; }
+    
+    /// <summary>
+    /// Email
+    /// </summary>
+    public string Email { get; set; }
+}
+
+/// <summary>
+/// class Model
+/// </summary>
+public class ClassGrade
+{
+    /// <summary>
+    /// ClassName
+    /// </summary>
+    public  string ClassName { get; set; }
+    
+    /// <summary>
+    /// ClassIndex
+    /// </summary>
+    public string  ClassIndex { get; set; }
+    
+    /// <summary>
+    /// 学生列表
+    /// </summary>
+    public List<Student> Students { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Bogus;
+
+Console.WriteLine("Analog data generation");
+var result = GetClassGrade();
+foreach (var grade in result)
+{
+    Console.WriteLine(grade.ClassName);
+    Console.WriteLine(grade.ClassIndex);
+    // foreach (var student in grade.Students)
+    // {
+    //      Console.WriteLine(student.Address);
+    //         Console.WriteLine(student.Age);
+    //         Console.WriteLine(student.Email);
+    //         Console.WriteLine(student.Id);
+    //         Console.WriteLine(student.Name);
+    // }
+    Console.WriteLine("******************");
+}
+
+Console.ReadKey();
+
+List<ClassGrade> GetClassGrade()
+{
+    var student =
+        new Faker<Student>()
+            .RuleFor(o => o.Address, f=>f.Person.Address.City)
+            .RuleFor(o => o.Age, f => f.IndexFaker)
+            .RuleFor(o => o.Id, Guid.NewGuid)
+            .RuleFor(o => o.Email, f => f.Person.Email)
+            .RuleFor(o => o.Name, f => f.Person.FullName);
+    var classGrade = new Faker<ClassGrade>().RuleFor(o => o.ClassName, f => f.Name.LastName())
+        .RuleFor(o => o.ClassIndex, f => f.Person.UserName)
+        .RuleFor(o => o.Students, f => student.Generate(50));
+    return classGrade.Generate(5);
+}
